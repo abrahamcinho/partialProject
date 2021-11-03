f5e48dbaf26e13270c9235c536ef60cd3846fc37
Abraham <abrahamcinho@gmail.com>
2021-11-03 11:52:14 -0300

    agregando responsive a login y registro, editando estilos

--- web/app.js
+++ web/app.js
@@ -2,22 +2,11 @@ const express = require("express");
 const server = express();
 const path = require("path");
 
-
 server.use("/public", express.static("public"));
 
 server.listen(3030, (req, res) => {
     console.log("Server para Sweet Dreams online...")
 });
-<<<<<<< HEAD
-
-const HOME_PAGE = "views/home.html";
-const PRODUCT_PAGE = "views/detalleDelProducto.html";
-const CART_PAGE = "views/carritoDeCompras.html";
-
-
-server.get("/", (req, res) =>{
-    res.sendFile(path.join(__dirname, HOME_PAGE))
-=======
 /*Declaramos las rutas de los .HTML como constantes*/
 const HOME_PAGE = "/views/home.html";
 const PRODUCT_PAGE = "/views/detalleDelProducto.html";
@@ -28,7 +17,6 @@ const REGISTER_PAGE = "/views/register.html";
 
 server.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, HOME_PAGE));
->>>>>>> de57a986a4d9921272a7f0e5ad0cb0019abf66be
 });
 server.get('/detalleDelProducto', (req, res) => {
     res.sendFile(path.join(__dirname, PRODUCT_PAGE));
@@ -36,12 +24,9 @@ server.get('/detalleDelProducto', (req, res) => {
 server.get('/carritoDeCompras', (req, res) => {
     res.sendFile(path.join(__dirname, CART_PAGE));
 });
-<<<<<<< HEAD
-=======
 server.get('/login', (req, res) => {
     res.sendFile(path.join(__dirname, LOGIN_PAGE));
 });
 server.get('/registro', (req, res) => {
     res.sendFile(path.join(__dirname, REGISTER_PAGE));
 });
->>>>>>> de57a986a4d9921272a7f0e5ad0cb0019abf66be
