08c5be562fedd7fdb7ed714fbbc69f6196547cda
CrisMarin58 <cgabrielamarin@gmail.com>
2021-11-24 21:26:20 -0300

    ruteo

--- web/app.js
+++ web/app.js
@@ -1,12 +1,12 @@
 const express = require("express");
-const server = express();
+const app = express();
 const path = require("path");
 
-server.use("/public", express.static("public"));
-server.set("views", path.resolve(__dirname, "views"));
-server.set("view engine", "ejs");
+app.listen(3030, (req, res) => console.log("Server para Sweet Dreams online..."));
 
-
+app.use("/public", express.static("public"));
+app.set("views", path.resolve(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const carritoCTRL = require("./controllers/carrito_Ctrl");
 const homeCTRL = require("./controllers/home_Ctrl");
@@ -17,45 +17,17 @@ const productosCTRL = require("./controllers/productos_Ctrl");
 const listadoProductosCTRL = require("./controllers/listadoProds_Ctrl");
 
 
-
-server.use("/carritoDeCompras", carritoCTRL);
-server.use("/", homeCTRL);
-server.use("/login", loginCTRL);
-server.use("/detalleDelProducto", detalleDelProductoCTRL);
-server.use("/registro", registerCTRL);
-server.use("/carga-edicion-productos", productosCTRL);
-server.use("/listado-productos", listadoProductosCTRL);
-
-
-server.listen(3030, (req, res) => {
-    console.log("Server para Sweet Dreams online...")
-});
+app.use("/carritoDeCompras", carritoCTRL);
+app.use("/", homeCTRL);
+app.use("/login", loginCTRL);
+app.use("/detalleDelProducto", detalleDelProductoCTRL);
+app.use("/registro", registerCTRL);
+app.use("/carga-edicion-productos", productosCTRL);
+app.use("/listado-productos", listadoProductosCTRL);
 
 
-
-
-
-
-/*Declaramos las rutas de los .HTML como constantes*/
-// const HOME_PAGE = "/views/home.ejs";
-// const PRODUCT_PAGE = "/views/detalleDelProducto.ejs";
-// const CART_PAGE = "/views/carritoDeCompras.ejs";
-// const LOGIN_PAGE = "/views/login.ejs";
-// const REGISTER_PAGE = "/views/register.ejs";
-
-
-// server.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, HOME_PAGE));
-// });
-// server.get('/detalleDelProducto', (req, res) => {
-//     res.sendFile(path.join(__dirname, PRODUCT_PAGE));
-// });
-// server.get('/carritoDeCompras', (req, res) => {
-//     res.sendFile(path.join(__dirname, CART_PAGE));
-// });
-// server.get('/login', (req, res) => {
-//     res.sendFile(path.join(__dirname, LOGIN_PAGE));
-// });
-// server.get('/registro', (req, res) => {
-//     res.sendFile(path.join(__dirname, REGISTER_PAGE));
-// });
+app.use('/', require('./routers/main'));
+app.use('/detalleDelProducto', require('./routers/productos'));
+app.use('/login', require('./routers/login'));
+app.use('/registro', require('./routers/register'));
+app.use('/carritoDeCompras', require('./routers/shoppingcart'));
