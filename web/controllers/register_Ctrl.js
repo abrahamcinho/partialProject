06bde47d2478da508061428c25e2cca52fc40161
CelesteSN <celesn3045@gmail>
2021-12-20 19:47:07 -0300

    Encripté el password y gestion de errores en registro de usuario

--- web/controllers/register_Ctrl.js
+++ web/controllers/register_Ctrl.js
@@ -5,25 +5,52 @@ var path = require("path");
 //Llamando al JSON
 const data = require("../data/usuarios.json");
 const dataPath = path.resolve(__dirname, "../data/usuarios.json")
+const bcryptjs = require("bcryptjs");
 
 router.get("/", (req, res) => {
     res.render("register");
 });
 //Registro de Usuario
 router.post("/", (req, res) => {
-    let usuario = {
-        id: data[data.length - 1].id + 1,
-        nombre: req.body.nombre,
-        apellido: req.body.apellido,
-        email: req.body.email,
-        password: req.body.password,
-        category: req.body.category
-    };
-    data.push(usuario);
-    var prodJson = JSON.stringify(data);
-    fs.writeFileSync(dataPath, prodJson);
-    //res.send("Registro Exitoso")
-    res.redirect("/login");
+    let usuario = findByField("email", req.body.email);
+    if (usuario) {
+        return res.render("register", {
+            errors: {
+                email: {
+                    msj: "Este email ya esta registrado"
+                }
+            },
+            oldData: req.body
+        });
+    } else {
+        usuario = {
+            id: data[data.length - 1].id + 1,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            email: req.body.email,
+            //password: req.body.password,
+            password: bcryptjs.hashSync(req.body.password, 10),
+            category: req.body.category
+        };
+
+        data.push(usuario);
+        var prodJson = JSON.stringify(data);
+        fs.writeFileSync(dataPath, prodJson);
+        //res.send("Registro Exitoso")
+        res.redirect("/login");
+
+    }
 });
+
+function findByField(field, text) {
+    let todosLosUsuarios = findAll();
+    let usuarioEncontrado = todosLosUsuarios.find(oneUser => oneUser[field] === text);
+    return usuarioEncontrado;
+
+}
+
+function findAll() {
+    return JSON.parse(fs.readFileSync(dataPath, "utf-8"));
+}
 
 module.exports = router;
--- web/routers/login.js
+++ web/routers/login.js
@@ -5,4 +5,10 @@ const LOGIN_PAGE = "/views/login.ejs";
 
 router.get('/', (req, res) => res.sendFile(path.join(__dirname, LOGIN_PAGE)));
 
+//Celeste
+// router.get("/login", usuarios_Ctrl.login);
+// router.post("/login", [check("email").isEmail().withMessaje("Email invalido"),
+//     check("password").isLength({ min: 8 }).withMessaje("La contraseña ingresada debe tener al menos 8 caracteres")
+// ], usuarios_Ctr.processLogin);
+
 module.exports = router;
